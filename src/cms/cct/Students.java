a5fc9963a257ca4a64cafbda3aa8c1b04ab1e6ae
markj6gar <mark.j6gar@gmail.com>
2024-04-04 02:22:17 +0100

    Creating Students object

--- src/cms/cct/Students.java
+++ src/cms/cct/Students.java
@@ -9,5 +9,33 @@ package cms.cct;
  * @author Mark
  */
 public class Students {
+    String student_id; 
+    String student_name;
+    String course_name;
+    String program_name;
+
+    public Students(String student_id, String student_name, String course_name, String program_name) {
+        this.student_id = student_id;
+        this.student_name = student_name;
+        this.course_name = course_name;
+        this.program_name = program_name;
+    }
+
+    public String getStudent_id() {
+        return student_id;
+    }
+
+    public String getStudent_name() {
+        return student_name;
+    }
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public String getProgram_name() {
+        return program_name;
+    }
+    
     
 }
