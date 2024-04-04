6465ff1e7b542aefb35fd2c563e3511c75877301
markj6gar <mark.j6gar@gmail.com>
2024-04-04 03:25:54 +0100

    creating object for enrollments

---
+++ src/cms/cct/Enrollments.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms.cct;
+
+/**
+ *
+ * @author Mark
+ */
+public class Enrollments {
+    String enrollment_id;
+    String student_id;
+    String course_code;
+    int semester;
+    int grade;
+
+    public Enrollments(String enrollment_id, String student_id, String course_code, int semester, int grade) {
+        this.enrollment_id = enrollment_id;
+        this.student_id = student_id;
+        this.course_code = course_code;
+        this.semester = semester;
+        this.grade = grade;
+    }
+
+    public String getEnrollment_id() {
+        return enrollment_id;
+    }
+
+    public String getStudent_id() {
+        return student_id;
+    }
+
+    public String getCourse_code() {
+        return course_code;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    
+    
+    
+    
+}
